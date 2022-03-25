3299bbfedc255816c93887f88bfc371658c4888f
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-03-25 16:33:25 +0000

    added code :)

--- main.cs
+++ main.cs
@@ -2,6 +2,7 @@
 // Created on: Mar 2022
 //
 // This program finds the area and perimeter of a triangle
+
 using System;
 
 class Program
@@ -12,15 +13,17 @@ class Program
         int Base;
         int Height;
 
-        Console.WriteLine("This program calculates the area of a triangle.");
+        Console.WriteLine("This program finds the area of a triangle.");
         Console.WriteLine("");
-        Console.Write("Enter the base (cm): ");
+        Console.Write("Enter the base of the triangle (cm): ");
         Base = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Enter the height (cm): ");
+        Console.Write("Enter the height of the triangle (cm): ");
         Height = Convert.ToInt32(Console.ReadLine());
-        Double Area = (Base * Height) /2 ;
+
+        Double Area = (Base * Height) / 2;
+
         Console.WriteLine("");
-        Console.Write("Area of the triangle = " + Area + " cm² ");
+        Console.Write("Area of the triangle = " + Area + " cm²");
         Console.ReadKey();
         Console.WriteLine("\nDone.");
     }
